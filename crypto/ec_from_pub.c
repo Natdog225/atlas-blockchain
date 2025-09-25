91ac3eb703410054143287ffe31a9a05d4e93b84
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-09-25 00:12:50 -0500

    fix

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -19,7 +19,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	if (!key)
 		return (NULL);
 
-	group = EC_KEY_get_0_group(key);
+	group = EC_KEY_get0_group(key);
 	point = EC_POINT_new(group);
 	if (!point)
 	{

7dc37b617ac4d3cec2ceceb3206dabcd613e1352
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-11-05 15:58:29 -0600

    Moved some stuff from A to B

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -1,29 +1,40 @@
 #ifndef BLOCKCHAIN_H
 #define BLOCKCHAIN_H
-#define BLOCK_DATA_MAX_LEN 1024
-#define BLOCK_GENERATION_INTERVAL 1
-#define DIFFICULTY_ADJUSTMENT_INTERVAL 5
 
-#include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 #include <llist.h>
-#include "../../crypto/hblk_crypto.h"
+#include <openssl/sha.h>
+#include "hblk_crypto.h"
+
+/*
+ * Include the transaction header
+ * This brings in all transaction-related structs and prototypes
+ */
 #include "transaction.h"
 
-/* MACRO for Genesis Block data length */
-#define BLOCKCHAIN_DATA_MAX 1024
+/* --- Constants (from v0.1/v0.2) --- */
+#define HBLK_MAGIC "HBLK"
+#define HBLK_VERSION "0.3"
+#define COINBASE_AMOUNT 50
+
+/* Maximum length for block data (from v0.2) */
+#define BLOCK_DATA_MAX 1024
+
+/* --- Block & Blockchain Structs (from v0.2, updated for v0.3) --- */
 
 /**
  * struct block_info_t - Block info structure
  *
- * @index:      Index of the Block in the Blockchain
- * @difficulty: Difficulty of a Block to be mined
- * @timestamp:  Time the Block was created at (UNIX timestamp)
- * @nonce:      Salt value used to alter the Block hash
- * @prev_hash:  Hash of the previous Block in the chain
+ * @index:       Block index
+ * @difficulty:  Block difficulty
+ * @timestamp:   Block timestamp
+ * @nonce:       Block nonce
+ * @prev_hash:   Hash of the previous block
  */
-typedef struct block_info_t
+typedef struct block_info
 {
 	uint32_t index;
 	uint32_t difficulty;
@@ -33,69 +44,38 @@ typedef struct block_info_t
 } block_info_t;
 
 /**
- * struct block_data_t - Block data
- *
- * @buffer: Data buffer
- * @len:    Data size (in bytes)
- */
-typedef struct block_data_t
-{
-	int8_t buffer[BLOCKCHAIN_DATA_MAX];
-	uint32_t len;
-} block_data_t;
-
-/**
  * struct block_t - Block structure
  *
- * @info: Block info
- * @data: Block data
- * @hash: 256-bit digest of the Block, to ensure authenticity
+ * @info:          Block info
+ * @data_len:      Length of the data
+ * @data:          Block data (flexible)
+ * @transactions:  List of transactions (NEW in v0.3)
+ * @hash:          Hash of the block
  */
-typedef struct block_t
+typedef struct block
 {
 	block_info_t info;
-	block_data_t data;
+	uint32_t data_len;
+	int8_t data[BLOCK_DATA_MAX];
+	llist_t *transactions; /* Updated for Task 10 */
 	uint8_t hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
 /**
  * struct blockchain_t - Blockchain structure
  *
- * @chain: Linked list of blocks
+ * @chain:   List of blocks
+ * @unspent: List of unspent transaction outputs (NEW in v0.3)
  */
-typedef struct blockchain_t
+typedef struct blockchain
 {
-	llist_t *chain;
+	llist_t *chain;	  /* list of block_t */
+	llist_t *unspent; /* Updated for Task 11 */
 } blockchain_t;
 
-/* --- Task 0 --- */
-tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+/* --- Blockchain/Block Function Prototypes --- */
+/* (These are from v0.2 and tasks 10, 11, 12, 13, 15) */
 
-/* --- Task 1 --- */
-unspent_tx_out_t *unspent_tx_out_create(
-	uint8_t block_hash[SHA256_DIGEST_LENGTH],
-	uint8_t tx_id[SHA256_DIGEST_LENGTH],
-	tx_out_t const *out
-);
-
-/* --- Task 2 --- */
-tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
-
-/* --- Task 3 --- */
-uint8_t *transaction_hash(
-	transaction_t const *transaction,
-	uint8_t hash_buf[SHA256_DIGEST_LENGTH]
-);
-
-/* --- Task 4 --- */
-sig_t *tx_in_sign(
-	tx_in_t *in,
-	uint8_t const tx_id[SHA256_DIGEST_LENGTH],
-	EC_KEY const *sender,
-	llist_t *all_unspent
-);
-
-/* Function Prototypes */
 blockchain_t *blockchain_create(void);
 block_t *block_create(block_t const *prev, int8_t const *data,
 					  uint32_t data_len);
@@ -105,7 +85,8 @@ uint8_t *block_hash(block_t const *block,
 					uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
-int block_is_valid(block_t const *block, block_t const *prev_block);
+int block_is_valid(block_t const *block, block_t const *prev_block,
+				   llist_t *all_unspent);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 							uint32_t difficulty);
 void block_mine(block_t *block);
--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <llist.h>
-#include "hblk_crypto.h"
+#include "hblk_crypto.h" /* For sig_t, EC_KEY, EC_PUB_LEN, etc. */
 
 /*
  * Defines a transaction output.
@@ -13,8 +13,8 @@
 typedef struct tx_out
 {
 	uint32_t amount;
-	uint8_t  pub[EC_PUB_LEN];
-	uint8_t  hash[SHA256_DIGEST_LENGTH];
+	uint8_t pub[EC_PUB_LEN];
+	uint8_t hash[SHA256_DIGEST_LENGTH];
 } tx_out_t;
 
 /*
@@ -23,8 +23,8 @@ typedef struct tx_out
  */
 typedef struct unspent_tx_out
 {
-	uint8_t	block_hash[SHA256_DIGEST_LENGTH];
-	uint8_t	tx_id[SHA256_DIGEST_LENGTH];
+	uint8_t block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t tx_id[SHA256_DIGEST_LENGTH];
 	tx_out_t out; /* The actual output, copied by value */
 } unspent_tx_out_t;
 
@@ -34,20 +34,61 @@ typedef struct unspent_tx_out
  */
 typedef struct tx_in
 {
-	uint8_t	block_hash[SHA256_DIGEST_LENGTH];
-	uint8_t	tx_id[SHA256_DIGEST_LENGTH];
-	uint8_t	tx_out_hash[SHA256_DIGEST_LENGTH];
-	sig_t   sig;
+	uint8_t block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t tx_id[SHA256_DIGEST_LENGTH];
+	uint8_t tx_out_hash[SHA256_DIGEST_LENGTH];
+	sig_t sig;
 } tx_in_t;
 
-/* Defines a transaction, contains input and outputs,things and stuff */
+/* Defines a transaction, containing inputs and outputs */
 typedef struct transaction
 {
 	uint8_t id[SHA256_DIGEST_LENGTH];
-	llist_t *inputs; /* list of tx_in_t */
+	llist_t *inputs;  /* list of tx_in_t */
 	llist_t *outputs; /* list of tx_out_t */
 } transaction_t;
 
-/* --- Function Prototypes --- */
+/* --- Transaction Function Prototypes --- */
+
+/* task 0 */
+tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+/* task 1 */
+unspent_tx_out_t *unspent_tx_out_create(
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	uint8_t tx_id[SHA256_DIGEST_LENGTH],
+	tx_out_t const *out);
+/* task 2 */
+tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
+/* task 3 */
+uint8_t *transaction_hash(
+	transaction_t const *transaction,
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+/* task 4 */
+sig_t *tx_in_sign(
+	tx_in_t *in,
+	uint8_t const tx_id[SHA256_DIGEST_LENGTH],
+	EC_KEY const *sender,
+	llist_t *all_unspent);
+/* task 5 */
+transaction_t *transaction_create(
+	EC_KEY const *sender,
+	EC_KEY const *receiver,
+	uint32_t amount,
+	llist_t *all_unspent);
+/* task 6 */
+int transaction_is_valid(
+	transaction_t const *transaction,
+	llist_t *all_unspent);
+/* task 7 */
+transaction_t *coinbase_create(EC_KEY const *receiver, uint32_t block_index);
+/* task 8 */
+int coinbase_is_valid(transaction_t const *coinbase, uint32_t block_index);
+/* task 9 */
+void transaction_destroy(transaction_t *transaction);
+/* task 14 */
+llist_t *update_unspent(
+	llist_t *transactions,
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	llist_t *all_unspent);
 
 #endif /* TRANSACTION_H */

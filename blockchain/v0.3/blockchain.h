5b0635e537b9165d57618b683568f5ef60c1058d
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-11-05 16:01:59 -0600

    Another update to the .h

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -8,22 +8,12 @@
 #include <llist.h>
 #include <openssl/sha.h>
 #include "hblk_crypto.h"
-
-/*
- * Include the transaction header
- * This brings in all transaction-related structs and prototypes
- */
 #include "transaction.h"
 
-/* --- Constants (from v0.1/v0.2) --- */
 #define HBLK_MAGIC "HBLK"
 #define HBLK_VERSION "0.3"
 #define COINBASE_AMOUNT 50
-
-/* Maximum length for block data (from v0.2) */
 #define BLOCK_DATA_MAX 1024
-
-/* --- Block & Blockchain Structs (from v0.2, updated for v0.3) --- */
 
 /**
  * struct block_info_t - Block info structure
@@ -40,55 +30,64 @@ typedef struct block_info
 	uint32_t difficulty;
 	uint64_t timestamp;
 	uint64_t nonce;
-	uint8_t prev_hash[SHA256_DIGEST_LENGTH];
+	uint8_t  prev_hash[SHA256_DIGEST_LENGTH];
 } block_info_t;
+
+/**
+ * struct block_data_t - Block data structure
+ *
+ * @buffer: Block data
+ * @len:    Length of the data
+ */
+typedef struct block_data
+{
+	int8_t   buffer[BLOCK_DATA_MAX];
+	uint32_t len;
+} block_data_t;
 
 /**
  * struct block_t - Block structure
  *
  * @info:          Block info
- * @data_len:      Length of the data
- * @data:          Block data (flexible)
- * @transactions:  List of transactions (NEW in v0.3)
+ * @data:          Block data
+ * @transactions:  List of transactions
  * @hash:          Hash of the block
  */
 typedef struct block
 {
 	block_info_t info;
-	uint32_t data_len;
-	int8_t data[BLOCK_DATA_MAX];
-	llist_t *transactions; /* Updated for Task 10 */
-	uint8_t hash[SHA256_DIGEST_LENGTH];
+	block_data_t data;
+	llist_t      *transactions;
+	uint8_t      hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
 /**
  * struct blockchain_t - Blockchain structure
  *
  * @chain:   List of blocks
- * @unspent: List of unspent transaction outputs (NEW in v0.3)
+ * @unspent: List of unspent transaction outputs
  */
 typedef struct blockchain
 {
-	llist_t *chain;	  /* list of block_t */
-	llist_t *unspent; /* Updated for Task 11 */
+	llist_t *chain;
+	llist_t *unspent;
 } blockchain_t;
 
 /* --- Blockchain/Block Function Prototypes --- */
-/* (These are from v0.2 and tasks 10, 11, 12, 13, 15) */
 
 blockchain_t *blockchain_create(void);
 block_t *block_create(block_t const *prev, int8_t const *data,
-					  uint32_t data_len);
+	uint32_t data_len);
 void block_destroy(block_t *block);
 void blockchain_destroy(blockchain_t *blockchain);
 uint8_t *block_hash(block_t const *block,
-					uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
 int block_is_valid(block_t const *block, block_t const *prev_block,
-				   llist_t *all_unspent);
+	llist_t *all_unspent);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
-							uint32_t difficulty);
+	uint32_t difficulty);
 void block_mine(block_t *block);
 uint32_t blockchain_difficulty(blockchain_t const *blockchain);
 

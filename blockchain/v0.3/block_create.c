643e4f1da812c07f4ab781afcecd16d77aac62b3
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-11-12 12:52:38 -0600

    Added lines

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -1,36 +1,38 @@
 #include "blockchain.h"
-#include <string.h>
-#include <time.h>
 
 /**
- * block_create - Creates a new block and initializes it.
- * @prev:       A pointer to the previous block in the blockchain.
- * @data:       The data to be duplicated into the block's data buffer.
- * @data_len:   The number of bytes to copy from `data`.
+ * block_create - creates a new Block
+ * @prev:     pointer to the previous Block in the chain
+ * @data:     data to store in the Block
+ * @data_len: length of @data
  *
- * Return: A pointer to the newly allocated block, or NULL on failure.
+ * Return: pointer to new Block, or NULL on failure
  */
-block_t *block_create(block_t const *prev,
-		      int8_t const *data, uint32_t data_len)
+block_t *block_create(block_t const *prev, int8_t const *data,
+					  uint32_t data_len)
 {
-	block_t *new_block;
-	uint32_t len_to_copy;
+	block_t *block;
+	uint32_t len;
 
-	if (!prev || !data)
+	block = calloc(1, sizeof(*block));
+	if (!block)
 		return (NULL);
 
-	new_block = calloc(1, sizeof(*new_block));
-	if (!new_block)
+	block->info.index = prev->info.index + 1;
+	block->info.timestamp = (uint64_t)time(NULL);
+	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+
+	len = data_len > BLOCK_DATA_MAX ? BLOCK_DATA_MAX : data_len;
+	memcpy(block->data.buffer, data, len);
+	block->data.len = len;
+
+	/* NEW LINE FOR TASK 10 */
+	block->transactions = llist_create(MT_SUPPORT_FALSE);
+	if (!block->transactions)
+	{
+		free(block);
 		return (NULL);
+	}
 
-	len_to_copy = data_len > BLOCKCHAIN_DATA_MAX ? BLOCKCHAIN_DATA_MAX : data_len;
-
-	new_block->info.index = prev->info.index + 1;
-	new_block->info.timestamp = time(NULL);
-	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
-
-	memcpy(new_block->data.buffer, data, len_to_copy);
-	new_block->data.len = len_to_copy;
-
-	return (new_block);
+	return (block);
 }
--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -1,11 +1,17 @@
 #include "blockchain.h"
 
 /**
- * block_destroy - Deletes a block.
- * @block: The block to be deleted.
+ * block_destroy - destroys a Block
+ * @block: pointer to the Block to destroy
  */
 void block_destroy(block_t *block)
 {
-	if (block)
-		free(block);
+	if (!block)
+		return;
+
+	/* NEW LINE FOR TASK 10 */
+	/* 1 = free content, (node_func_t)transaction_destroy = free function */
+	llist_destroy(block->transactions, 1, (node_func_t)transaction_destroy);
+
+	free(block);
 }

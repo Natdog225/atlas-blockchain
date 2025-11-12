00a0b6a8eaf8e8d1bde1ef272bc7561c1b10eeb7
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-11-12 12:35:38 -0600

    Slight changes to 6 to test

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -3,7 +3,6 @@
 
 /**
  * find_unspent_output - Finds an unspent output matching a transaction input
- * (This is the static helper from tx_in_sign.c, repeated here for use)
  * @node: Pointer to the unspent_tx_out_t node
  * @in:   Pointer to the tx_in_t to match
  *
@@ -14,6 +13,7 @@ static int find_unspent_output(llist_node_t node, void *in)
 	unspent_tx_out_t *utxo = node;
 	tx_in_t *tx_in = in;
 
+	/* Compare the 3-tuple that uniquely identifies a UTXO */
 	if (memcmp(utxo->block_hash, tx_in->block_hash, SHA256_DIGEST_LENGTH) == 0 &&
 		memcmp(utxo->tx_id, tx_in->tx_id, SHA256_DIGEST_LENGTH) == 0 &&
 		memcmp(utxo->out.hash, tx_in->tx_out_hash, SHA256_DIGEST_LENGTH) == 0)

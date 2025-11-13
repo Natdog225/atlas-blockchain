35678297d18072840ef148f1a61eeb9a94e8fa84
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-11-13 00:08:45 -0600

    Potential fix

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -1,77 +1,163 @@
 #include "blockchain.h"
-#include <string.h>
+#include <string.h> /* For memcmp */
+#include <stdio.h>  /* For fprintf, stderr */
 
 /**
- * find_unspent_output - Finds an unspent output matching a transaction input
+ * calculate_output_amount - calculates the total amount of all outputs
+ * @transaction: transaction to verify
+ *
+ * Return: sum of output amounts
  */
-static int find_unspent_output(llist_node_t node, void *in)
+uint32_t calculate_output_amount(transaction_t const *transaction)
 {
-	unspent_tx_out_t *utxo = node;
-	tx_in_t *tx_in = in;
+	int i, out_size;
+	uint32_t output_amount = 0;
+	tx_out_t *out = NULL;
 
-	/* A UTXO is uniquely identified by the hash of the output */
-	if (memcmp(utxo->out.hash, tx_in->tx_out_hash, SHA256_DIGEST_LENGTH) == 0)
+	/*
+	 * FIX 1: This must be a loop to sum ALL outputs,
+	 * not just the first two.
+	 */
+	out_size = llist_size(transaction->outputs);
+	for (i = 0; i < out_size; i++)
 	{
-		return (0);
+		out = (tx_out_t *)llist_get_node_at(transaction->outputs, i);
+		if (out)
+			output_amount += out->amount;
+	}
+
+	return (output_amount);
+}
+
+/**
+ * check_unspent_vs_input - checks whether an unspent tx is part of the input
+ * @unspent: individual unspent transaction
+ * @input: individual input transaction
+ *
+ * Return: 1 if matching, 0 otherwise
+ */
+int check_unspent_vs_input(unspent_tx_out_t *unspent, tx_in_t *input)
+{
+	if ((memcmp(input->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH)
+		== 0) &&
+		(memcmp(input->tx_id, unspent->tx_id, SHA256_DIGEST_LENGTH) == 0) &&
+		(memcmp(input->tx_out_hash, unspent->out.hash, SHA256_DIGEST_LENGTH)
+		== 0))
+		return (1);
+	return (0);
+}
+
+/**
+ * verify_inputs - verifies inputs used in transaction vs unspent
+ * @transaction: transaction to verify
+ * @all_unspent: is the list of all unspent transaction outputs to date
+ * @input_amount: sum of matching amounts in inputs
+ *
+ * Return: 1 on sucess, 0 otherwise
+ */
+int verify_inputs(transaction_t const *transaction, llist_t *all_unspent,
+	uint32_t *input_amount)
+{
+	int i, j, inputs_size, all_unspent_size, found;
+	tx_in_t *input = NULL;
+	unspent_tx_out_t *unspent = NULL;
+	EC_KEY *unspent_key = NULL;
+
+	inputs_size = llist_size(transaction->inputs);
+	all_unspent_size = llist_size(all_unspent);
+
+	for (i = 0; i < inputs_size; i++)
+	{
+		input = (tx_in_t *)llist_get_node_at(transaction->inputs, i);
+		if (!input)
+			return (0); /* Should not happen, but good to check */
+		
+		found = 0;
+		for (j = 0; j < all_unspent_size; j++)
+		{
+			unspent = (unspent_tx_out_t *)llist_get_node_at(all_unspent, j);
+			if (!unspent)
+				continue;
+
+			if (check_unspent_vs_input(unspent, input))
+			{
+				found = 1;
+				/*
+				 * FIX: Create and free the key *only* when a match is found
+				 * to prevent memory leaks or double-frees.
+				 */
+				unspent_key = ec_from_pub(unspent->out.pub);
+				if (!unspent_key)
+					return (0); /* Key creation failed */
+				
+				if (!ec_verify(unspent_key, transaction->id,
+					SHA256_DIGEST_LENGTH, &input->sig))
+				{
+					fprintf(stderr, "!ec_verify\n");
+					EC_KEY_free(unspent_key);
+					return (0);
+				}
+				*input_amount += unspent->out.amount;
+				EC_KEY_free(unspent_key); /* Free the key after use */
+				break; /* Stop inner loop, move to next input */
+			}
+		}
+		if (!found)
+		{
+			fprintf(stderr, "!found\n");
+			return (0);
+		}
 	}
 	return (1);
 }
 
+/**
+ * transaction_is_valid - checks whether a transaction is valid
+ * @transaction: transaction to verify
+ * @all_unspent: is the list of all unspent transaction outputs to date
+ *
+ * Return: 1 if the transaction is valid, 0 otherwise
+ */
 int transaction_is_valid(transaction_t const *transaction,
-						 llist_t *all_unspent)
+	llist_t *all_unspent)
 {
-	uint8_t hash_buf[SHA256_DIGEST_LENGTH];
-	int i, num_inputs, num_outputs;
-	uint32_t total_in = 0, total_out = 0;
-	tx_in_t *in;
-	tx_out_t *out;
-	unspent_tx_out_t *utxo;
-	EC_KEY *temp_key;
+	uint8_t test_hash[SHA256_DIGEST_LENGTH] = {0};
+	uint32_t output_amount = 0, input_amount = 0;
 
-	if (!transaction || !all_unspent || !transaction->inputs || !transaction->outputs)
-		return (0);
-
-	if (!transaction_hash(transaction, hash_buf) ||
-		memcmp(hash_buf, transaction->id, SHA256_DIGEST_LENGTH) != 0)
-		return (0);
-
-	num_inputs = llist_size(transaction->inputs);
-	num_outputs = llist_size(transaction->outputs);
-
-	if (num_inputs == 0)
-		return (0);
-
-	for (i = 0; i < num_inputs; i++)
+	/*
+	 * FIX: Add NULL checks for inputs/outputs to prevent segfaults
+	 * on malformed transactions.
+	 */
+	if (!transaction || !all_unspent || !transaction->inputs ||
+		!transaction->outputs)
 	{
-		in = llist_get_node_at(transaction->inputs, i);
-		if (!in)
-			return (0);
-		
-		utxo = llist_find_node(all_unspent, find_unspent_output, in);
-		if (!utxo)
-			return (0);
-		
-		temp_key = ec_from_pub(utxo->out.pub);
-		if (!temp_key ||
-			!ec_verify(temp_key, transaction->id, SHA256_DIGEST_LENGTH, &in->sig))
-		{
-			EC_KEY_free(temp_key);
-			return (0);
-		}
-		EC_KEY_free(temp_key);
-		total_in += utxo->out.amount;
+		fprintf(stderr, "!params\n");
+		return (0);
 	}
 
-	for (i = 0; i < num_outputs; i++)
+	transaction_hash(transaction, test_hash);
+	if (memcmp(test_hash, transaction->id, SHA256_DIGEST_LENGTH) != 0)
 	{
-		out = llist_get_node_at(transaction->outputs, i);
-		if (!out)
-			return (0);
-		total_out += out->amount;
+		fprintf(stderr, "!transaction hash\n");
+		return (0);
 	}
 
-	if (total_in != total_out)
+	if (llist_size(transaction->inputs) == 0)
+		return (0); /* Regular transactions must have inputs */
+
+	if (!verify_inputs(transaction, all_unspent, &input_amount))
 		return (0);
+
+	output_amount = calculate_output_amount(transaction);
+
+	if (output_amount != input_amount)
+	{
+		fprintf(stderr, "outputs != inputs outputs: %d, inputs: %d\n",
+			output_amount, input_amount);
+		fprintf(stderr, "output_size: %d, input_size: %d\n",
+			llist_size(transaction->outputs), llist_size(transaction->inputs));
+		return (0);
+	}
 
 	return (1);
 }

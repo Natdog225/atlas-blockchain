894040e12fb1b7b48215c3f1d964f0d90e6a2bed
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-11-12 22:25:41 -0600

    Anger

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -1,11 +1,16 @@
 #include "blockchain.h"
+#include <string.h>
 
-extern block_t const _genesis_block;
-
+/**
+ * blockchain_create - Creates a Blockchain structure and initializes it with a
+ * Genesis Block.
+ *
+ * Return: A pointer to the newly created blockchain, or NULL on failure.
+ */
 blockchain_t *blockchain_create(void)
 {
 	blockchain_t *blockchain;
-	block_t *genesis;
+	block_t *genesis_block;
 
 	blockchain = calloc(1, sizeof(*blockchain));
 	if (!blockchain)
@@ -18,11 +23,36 @@ blockchain_t *blockchain_create(void)
 		return (NULL);
 	}
 
-	genesis = (block_t *)&_genesis_block;
-
-	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_REAR) != 0)
+	/* THIS IS THE VERSION THAT ALLOCATES GENESIS */
+	genesis_block = calloc(1, sizeof(*genesis_block));
+	if (!genesis_block)
 	{
 		llist_destroy(blockchain->chain, 0, NULL);
+		free(blockchain);
+		return (NULL);
+	}
+
+	genesis_block->info.index = 0;
+	genesis_block->info.difficulty = 0;
+	genesis_block->info.timestamp = 1537578000;
+	genesis_block->info.nonce = 0;
+	memset(genesis_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
+
+	memcpy(genesis_block->data.buffer, "Holberton School", 16);
+	genesis_block->data.len = 16;
+	genesis_block->transactions = NULL; /* Genesis has NULL tx list */
+
+	memcpy(genesis_block->hash,
+		   "\xc5\x2c\x26\xc8\xb5\x46\x16\x39"
+		   "\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d"
+		   "\x0c\x8e\x00\x09\xc8\x17\xf2\xb1"
+		   "\xd3\xd7\xff\x2f\x04\x51\x58\x03",
+		   SHA256_DIGEST_LENGTH);
+
+	if (llist_add_node(blockchain->chain, genesis_block, ADD_NODE_FRONT) != 0)
+	{
+		llist_destroy(blockchain->chain, 0, NULL);
+		free(genesis_block);
 		free(blockchain);
 		return (NULL);
 	}
@@ -30,7 +60,7 @@ blockchain_t *blockchain_create(void)
 	blockchain->unspent = llist_create(MT_SUPPORT_FALSE);
 	if (!blockchain->unspent)
 	{
-		llist_destroy(blockchain->chain, 0, NULL);
+		llist_destroy(blockchain->chain, 1, (void (*)(void *))block_destroy);
 		free(blockchain);
 		return (NULL);
 	}
--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -5,8 +5,6 @@ void blockchain_destroy(blockchain_t *blockchain)
 	if (!blockchain)
 		return;
 
-	llist_pop(blockchain->chain);
-
 	llist_destroy(blockchain->chain, 1, (void (*)(void *))block_destroy);
 	
 	llist_destroy(blockchain->unspent, 1, free);

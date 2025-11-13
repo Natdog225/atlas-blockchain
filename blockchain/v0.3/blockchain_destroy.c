764d53a20e5926bade653adf7ed79697b61a9048
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-11-12 21:55:45 -0600

    Another attempt at a fix

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -11,20 +11,26 @@ void blockchain_destroy(blockchain_t *blockchain)
 	if (!blockchain)
 		return;
 
-	while (llist_size(blockchain->chain) > 1)
-	{
-		block = llist_get_node_at(blockchain->chain,
-			llist_size(blockchain->chain) - 1);
-		
-		llist_remove_node(blockchain->chain, llist_size(blockchain->chain) - 1, 0);
+	/*
+	 * Pop the genesis block node.
+	 */
+	llist_pop(blockchain->chain);
 
+	/*
+	 * Pop the next block (the one allocated on the heap).
+	 */
+	block = llist_pop(blockchain->chain);
+	if (block)
+	{
 		block_destroy(block);
 	}
 
-	llist_pop(blockchain->chain);
-
+	/* Now that the chain list is empty, destroy it */
 	llist_destroy(blockchain->chain, 0, NULL);
+	
+	/* Destroy the unspent list */
 	llist_destroy(blockchain->unspent, 1, free);
 
+	/* Finally, free the blockchain struct itself */
 	free(blockchain);
 }

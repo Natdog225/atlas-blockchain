ac5c80cb54d29cd4ad951304e1d4ebd7d01268e8
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-11-12 22:13:38 -0600

    back

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -2,29 +2,14 @@
 
 void blockchain_destroy(blockchain_t *blockchain)
 {
-	block_t *block;
-
 	if (!blockchain)
 		return;
 
-	/*
-	 * Manually pop and destroy the heap-allocated block
-	 */
-	block = llist_pop(blockchain->chain);
-	if (block)
-	{
-		block_destroy(block);
-	}
-	
-	/*
-	 * Manually pop the const genesis block.
-	 */
 	llist_pop(blockchain->chain);
 
-	/* Destroy the (now empty) lists */
-	llist_destroy(blockchain->chain, 0, NULL);
+	llist_destroy(blockchain->chain, 1, (void (*)(void *))block_destroy);
+	
 	llist_destroy(blockchain->unspent, 1, free);
 
-	/* Free the blockchain struct itself */
 	free(blockchain);
 }
